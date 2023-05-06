ac089a42002687487575ff5f8e0182ceb6bb2eb5
Dan Cross <cross@gajendra.net>
2023-05-06 19:50:01 +0000

    usertests: Make mem test faster
    This was painfully slow on a big-memory machine.
    Increase the allocation sizes to make it reasonable.
    
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- cmd/malloctest.c
+++ cmd/malloctest.c
@@ -20,7 +20,7 @@ mem(void)
   ppid = getpid();
   if((pid = fork()) == 0){
     m1 = 0;
-    while((m2 = malloc(10001)) != 0){
+    while((m2 = malloc(1000001)) != 0){
       *(char**)m2 = m1;
       m1 = m2;
     }
@@ -29,7 +29,7 @@ mem(void)
       free(m1);
       m1 = m2;
     }
-    m1 = malloc(1024*20);
+    m1 = malloc(1024*1024*2+1);
     if(m1 == 0){
       printf("couldn't allocate mem?!!\n");
       kill(ppid, SIGTERM);
